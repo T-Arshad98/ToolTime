53b370da680948d2de565a6e1836b43171c8c808
Taimoor Arshad <113066434+tarshad2@users.noreply.github.com>
2025-07-04 21:23:47 -0600

    Initial commit

---
+++ ToolTime/Models/Tool.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ToolTime.Models
+{
+    public class Tool
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Type { get; set; }
+
+        [Required]
+        public required string SerialNumber { get; set; }
+
+        [AllowNull]
+        public string? Description { get; set; }
+
+        [AllowNull]
+        public DateTime? LastInspectionDate { get; set; }
+    }
+}
